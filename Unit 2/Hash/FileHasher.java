347e6ead2975d2f233728e48e5257dd71b17800b
TheGreenCoconut <quinn.harris@icloud.com>
2026-09-11 13:22:48 -0700

    (FH-3): Program reads files back and builds backup.txt

--- Unit 2/Hash/FileHasher.java
+++ Unit 2/Hash/FileHasher.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -7,19 +8,41 @@ public class FileHasher {
         File f = new File("./JavaFileSystem");
         f.mkdir();
 
-        File notes = new File("./JavaFileSystem/notes.txt");
-        FileWriter writer = new FileWriter(notes);
-        writer.write("These are some of the best notes I've ever written");
-        writer.close();
-        
-        File data = new File("./JavaFileSystem/data.txt");
-        writer = new FileWriter(data);
-        writer.write("Man take a look at all of this data");
-        writer.close();
+        writeFile("JavaFileSystem", "notes.txt", "Wow these notes are fire");
+        writeFile("JavaFileSystem", "data.txt", "Man take a look at all of this data");
+        writeFile("JavaFileSystem", "log.txt", "Whatever you were doing before really sucked");
 
-        File log = new File("./JavaFileSystem/log.txt");
-        writer = new FileWriter(log);
-        writer.write("Whatever you were doing before really sucked");
-        writer.close();
+        makeBackup(f);
+    }
+
+    private static void writeFile(String directory, String file, String content) {
+        try {
+            File f = new File("./" + directory + "/" + file);
+            FileWriter writer = new FileWriter(f);
+            writer.write(content);
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("File Creation Failed");
+        }
+    }
+
+    private static void makeBackup(File directory){
+        try {
+            File backup = new File("./" + directory.getName() + "/backup.txt");
+            FileWriter writer = new FileWriter(backup);
+            for (File file : directory.listFiles()) {
+                if (file.getName().equals("backup.txt") || file.getName().equals(".DS_Store")) continue;
+                System.out.println(file.getName());
+                writer.write(file.getName() + ": ");
+                FileReader reader = new FileReader(file);
+                int c;
+                while ((c = reader.read()) != -1) {
+                    writer.write((char) (c));
+                }
+                reader.close();
+                writer.write("\n");
+            }   
+            writer.close();
+        } catch (Exception e){}
     }
 }
